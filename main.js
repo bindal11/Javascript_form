618188e064d01e1f7c9db29ce81b19e937cafdf4
Bhavika <brbindal@tekion.com>
2023-06-20 10:00:34 +0530

    form changes

---
+++ main.js
@@ -0,0 +1,27 @@
+// let productList = null;
+
+// const fetchProductDetails = () => {
+//     productList = fetch('https://fakestoreapi.com/products?sort=asc').then((data) => data.json()).then((newData) => newData);
+
+// }
+
+// const populateProductList = () =>  {
+// productList.map((item) => )
+  
+// }
+
+
+// fetchProductDetails();
+
+
+var myForm = document.getElementById("myForm");
+var result = document.getElementById("result");
+function test () {
+    console.log("777");
+}
+function submitForm(event) {
+    console.log("888", event)
+        //  event.preventDefault();
+        //  result.innerHTML = "<b>The button is pressed and form is submitted.</b>"
+        
+      }
